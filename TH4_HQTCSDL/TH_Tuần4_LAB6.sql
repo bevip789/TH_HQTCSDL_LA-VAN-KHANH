a86bfc84f4a2c3774f6d79abb527aac94e221ef4
vankhanh <88921350+vankhanh4742@users.noreply.github.com>
2023-04-12 11:17:14 +0700

    Add files via upload

---
+++ TH4_HQTCSDL/TH_Tuần4_LAB6.sql
@@ -0,0 +1,177 @@
+﻿-- câu 1
+CREATE FUNCTION find_sanpham
+(
+    @TenHang NVARCHAR(50)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT
+        sp.masp AS 'MaSP',
+        sp.tensp AS 'TenSP',
+        hs.tenhang AS 'TenHang',
+        sp.soluong AS 'SoLuong',
+        sp.mausac AS 'MauSac',
+        sp.giaban AS 'GiaBan',
+        sp.donvitinh AS 'DonViTinh',
+        sp.mota AS 'MoTa'
+    FROM
+        Sanpham sp
+    INNER JOIN
+        Hangsx hs ON sp.mahangsx = hs.mahangsx
+    WHERE
+        hs.tenhang LIKE '%' + @TenHang + '%'
+);
+
+
+-- câu2
+CREATE FUNCTION find_sanpham_hang
+(
+    @NgayBatDau DATE,
+    @NgayKetThuc DATE
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT
+        sp.masp AS 'MaSP',
+        sp.tensp AS 'TenSP',
+        hs.tenhang AS 'TenHang',
+        sp.soluong AS 'SoLuong',
+        sp.mausac AS 'MauSac',
+        sp.giaban AS 'GiaBan',
+        sp.donvitinh AS 'DonViTinh',
+        sp.mota AS 'MoTa',
+        np.ngaynhap AS 'NgayNhap'
+    FROM
+        Nhap np
+    INNER JOIN
+        Sanpham sp ON np.masp = sp.masp
+    INNER JOIN
+        Hangsx hs ON sp.mahangsx = hs.mahangsx
+    WHERE
+        np.ngaynhap BETWEEN @NgayBatDau AND @NgayKetThuc
+);
+
+-- câu 3
+CREATE FUNCTION listSanPham
+(
+    @MaHangSX NCHAR(10),
+    @LuaChon INT
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT Sanpham.masp, Sanpham.mahangsx, Sanpham.tensp, Sanpham.soluong, Sanpham.mausac, Sanpham.giaban, Sanpham.donvitinh, Sanpham.mota
+    FROM Sanpham
+    WHERE Sanpham.mahangsx = @MaHangSX
+    AND (@LuaChon = 0 AND Sanpham.soluong = 0 OR @LuaChon = 1 AND Sanpham.soluong > 0)
+);
+
+
+
+--câu 4
+CREATE FUNCTION listNhanvien
+(
+    @TenPhong NVARCHAR(50)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT Nhanvien.manv, Nhanvien.tennv, Nhanvien.gioitinh, Nhanvien.diachi, Nhanvien.sodt, Nhanvien.email, Nhanvien.phong
+    FROM Nhanvien
+    WHERE Nhanvien.phong = @TenPhong
+);
+
+
+-- câu5
+CREATE FUNCTION listHangsxLike
+(
+    @DiaChi NVARCHAR(255)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT Hangsx.mahangsx, Hangsx.tenhang, Hangsx.diachi, Hangsx.sodt, Hangsx.email
+    FROM Hangsx
+    WHERE Hangsx.diachi LIKE '%' + @DiaChi + '%'
+);
+
+
+
+-- câu 6
+CREATE FUNCTION listsanpham_year
+(
+    @NamBatDau INT,
+    @NamKetThuc INT
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT Sanpham.masp, Sanpham.mahangsx, Sanpham.tensp, Hangsx.tenhang
+    FROM Sanpham
+    INNER JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+    INNER JOIN Nhap ON Sanpham.masp = Nhap.masp
+	WHERE YEAR(Nhap.ngaynhap) BETWEEN @NamBatDau AND @NamKetThuc
+);
+
+
+-- câu 7
+CREATE FUNCTION listNhapOrXuat
+(
+    @GiaBatDau DECIMAL(18, 2),
+    @GiaKetThuc DECIMAL(18, 2),
+    @TenHangSX NVARCHAR(100)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT Sanpham.masp, Sanpham.mahangsx, Sanpham.tensp, Sanpham.giaban
+    FROM Sanpham
+    INNER JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+    WHERE Sanpham.giaban BETWEEN @GiaBatDau AND @GiaKetThuc
+    AND Hangsx.tenhang = @TenHangSX
+);
+
+-- câu 8
+CREATE FUNCTION listnhanvienN
+(
+    @NgayNhap DATE
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT nhanvien.*
+    FROM Nhap
+    INNER JOIN Nhanvien ON Nhap.manv = Nhanvien.manv
+    WHERE Nhap.ngaynhap = @NgayNhap
+);
+
+-- câu 9
+
+CREATE FUNCTION listSanPhamXYZ
+(
+    @Min FLOAT,
+    @Max FLOAT,
+    @CongTySanXuat NVARCHAR(100)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT Sanpham.*, Hangsx.tenhang
+    FROM Sanpham
+    INNER JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+    WHERE Sanpham.giaban BETWEEN @Min AND @Max
+    AND Hangsx.tenhang = @CongTySanXuat
+);
+
+
---
+++ TH4_HQTCSDL/TH_Tuần4_LAB7.sql
@@ -0,0 +1,209 @@
+﻿-- câu 1
+CREATE PROCEDURE InsertHangsx
+    @mahangsx NCHAR(10),
+    @tenhang NVARCHAR(20),
+    @diachi NVARCHAR(30),
+    @sodt NVARCHAR(20),
+    @email NVARCHAR(30)
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM Hangsx WHERE tenhang = @tenhang)
+    BEGIN
+        PRINT 'Tên hàng dã tồn tại trên cơ sở dữ liệu'
+    END
+    ELSE
+    BEGIN
+        INSERT INTO Hangsx (mahangsx, tenhang, diachi, sodt, email)
+        VALUES (@mahangsx, @tenhang, @diachi, @sodt, @email)
+    END
+END
+
+-- câu 2
+CREATE PROCEDURE InsertOrUpdateSanPham
+    @masp NCHAR(10),
+    @mahangsx NCHAR(10),
+    @tensp NVARCHAR(20),
+    @soluong INT,
+    @mausac NVARCHAR(20),
+    @giaban MONEY,
+    @donvitinh NCHAR(10),
+    @mota NVARCHAR(MAX)
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM SanPham WHERE masp = @masp)
+    BEGIN
+        UPDATE SanPham
+        SET mahangsx = @mahangsx,
+            tensp = @tensp,
+            soluong = @soluong,
+            mausac = @mausac,
+            giaban = @giaban,
+            donvitinh = @donvitinh,
+            mota = @mota
+        WHERE masp = @masp
+    END
+    ELSE
+    BEGIN
+        INSERT INTO SanPham (masp, mahangsx, tensp, soluong, mausac, giaban, donvitinh, mota)
+        VALUES (@masp, @mahangsx, @tensp, @soluong, @mausac, @giaban, @donvitinh, @mota)
+    END
+END
+
+-- câu3
+CREATE PROCEDURE DeleteHangsx
+    @tenhang NVARCHAR(20)
+AS
+BEGIN
+    IF NOT EXISTS (SELECT * FROM Hangsx WHERE tenhang = @tenhang)
+    BEGIN
+        PRINT N'Ten hang không tồn tại'
+    END
+    ELSE
+    BEGIN
+        DELETE FROM SanPham WHERE mahangsx IN (SELECT mahangsx FROM Hangsx WHERE tenhang = @tenhang)
+        DELETE FROM Hangsx WHERE tenhang = @tenhang
+    END
+END
+
+-- câu4
+CREATE PROCEDURE InsertOrUpdateNhanVien
+    @manv NCHAR(10),
+    @tennv NVARCHAR(20),
+    @gioitinh NCHAR(10),
+    @diachi NVARCHAR(30),
+    @sodt NVARCHAR(20),
+    @email NVARCHAR(30),
+    @phong NVARCHAR(30),
+    @Flag BIT
+AS
+BEGIN
+    IF @Flag = 0
+    BEGIN
+        UPDATE NhanVien
+        SET tennv = @tennv,
+            gioitinh = @gioitinh,
+            diachi = @diachi,
+            sodt = @sodt,
+            email = @email,
+            phong = @phong
+        WHERE manv = @manv
+    END
+    ELSE
+    BEGIN
+        INSERT INTO NhanVien (manv, tennv, gioitinh, diachi, sodt, email, phong)
+        VALUES (@manv, @tennv, @gioitinh, @diachi, @sodt, @email, @phong)
+    END
+END
+
+--câu5
+CREATE PROCEDURE InsertOrUpdateNhap
+    @sohdn NCHAR(10),
+    @masp NCHAR(10),
+    @manv NCHAR(10),
+    @ngaynhap DATE,
+    @soluongN INT,
+    @dongiaN MONEY
+AS
+BEGIN
+    IF NOT EXISTS (SELECT * FROM SanPham WHERE masp = @masp)
+    BEGIN
+        PRINT N'mã sản phẩm không tồn tại trong bảng SanPham '
+    END
+    ELSE IF NOT EXISTS (SELECT * FROM NhanVien WHERE manv = @manv)
+    BEGIN
+        PRINT N'mã nhân viên không tồn tại trong bảng NhanVien '
+    END
+    ELSE
+    BEGIN
+        IF EXISTS (SELECT * FROM Nhap WHERE sohdn = @sohdn)
+        BEGIN
+            UPDATE Nhap
+            SET masp = @masp,
+                manv = @manv,
+                ngaynhap = @ngaynhap,
+                soluongN = @soluongN,
+                dongiaN = @dongiaN
+            WHERE sohdn = @sohdn
+        END
+        ELSE
+        BEGIN
+            INSERT INTO Nhap (sohdn, masp, manv, ngaynhap, soluongN, dongiaN)
+            VALUES (@sohdn, @masp, @manv, @ngaynhap, @soluongN, @dongiaN)
+        END
+    END
+END
+
+-- câu 6
+CREATE PROCEDURE InsertOrUpdateHoaDonXuat
+    @sohdx NCHAR(10),
+    @masp NCHAR(10),
+    @manv NCHAR(10),
+    @ngayxuat DATE,
+    @soluongX INT
+AS
+BEGIN
+    IF NOT EXISTS (SELECT * FROM SanPham WHERE masp = @masp)
+    BEGIN
+        PRINT 'mã sản phẩm không tồn tại trong bảng SanPham '
+    END
+    ELSE IF NOT EXISTS (SELECT * FROM NhanVien WHERE manv = @manv)
+    BEGIN
+        PRINT N'mã nhân viên không tồn tại trong bảng NhanVien '
+    END
+    ELSE IF NOT EXISTS (SELECT * FROM SanPham WHERE masp = @masp AND soluong >= @soluongX)
+    BEGIN
+        PRINT N'Số lượng xuất lớn hơn số lượng có trong bảng SanPham'
+    END
+    ELSE
+    BEGIN
+        IF EXISTS (SELECT * FROM Xuat WHERE sohdx = @sohdx)
+        BEGIN
+            UPDATE Xuat
+            SET masp = @masp,
+                manv = @manv,
+                ngayxuat = @ngayxuat,
+                soluongX = @soluongX
+            WHERE sohdx = @sohdx
+        END
+        ELSE
+        BEGIN
+            INSERT INTO Xuat (sohdx, masp, manv, ngayxuat, soluongX)
+            VALUES (@sohdx, @masp, @manv, @ngayxuat, @soluongX)
+        END
+    END
+END
+
+-- câu 7
+CREATE PROCEDURE DeleteNhanVien
+    @manv NCHAR(10)
+AS
+BEGIN
+    IF NOT EXISTS (SELECT * FROM NhanVien WHERE manv = @manv)
+    BEGIN
+        PRINT N'Manv không tồn tại'
+    END
+    ELSE
+    BEGIN
+        DELETE FROM Nhap WHERE manv = @manv
+        DELETE FROM Xuat WHERE manv = @manv
+        DELETE FROM NhanVien WHERE manv = @manv
+    END
+END
+
+
+-- câu 8
+CREATE PROCEDURE DeleteSanPham
+    @masp NCHAR(10)
+AS
+BEGIN
+    IF NOT EXISTS (SELECT * FROM SanPham WHERE masp = @masp)
+    BEGIN
+        PRINT N'Masp không tồn tại'
+    END
+    ELSE
+    BEGIN
+        DELETE FROM Nhap WHERE masp = @masp
+        DELETE FROM Xuat WHERE masp = @masp
+        DELETE FROM SanPham WHERE masp = @masp
+    END
+END
